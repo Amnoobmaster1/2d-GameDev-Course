82a7d8071e2c0a0ffb93c21c8de5669a5a5cc354
Amnoobmaster1 <159198065+Amnoobmaster1@users.noreply.github.com>
2024-04-05 15:43:27 -0700

    M5 L.6

--- 05.loot_it_all_workbook/practices/L6.P2.coins_and_energy_packs/ship_coins_and_energy_packs.gd
+++ 05.loot_it_all_workbook/practices/L6.P2.coins_and_energy_packs/ship_coins_and_energy_packs.gd
@@ -10,7 +10,14 @@ var target_position := Vector2(0, 0)
 var coins := 0
 var energy := 20
 
+func set_coin_count(new_coin_count: int) -> void:
+	coins = new_coin_count
+	get_node("UI/CoinsCount").text = "x" + str(coins)
 
+func set_energy(new_energy: int) -> void:
+	energy = new_energy
+	get_node("UI/EnergyBar").value = energy
+	
 func _ready() -> void:
 	area_entered.connect(_on_area_entered)
 	set_process(false)
@@ -36,5 +43,9 @@ func _process(delta: float) -> void:
 
 
 func _on_area_entered(area_that_entered: Area2D) -> void:
-	energy += 20.0
-	get_node("UI/EnergyBar").value = energy
+	if area_that_entered.is_in_group("coin"):
+		set_coin_count(coins + 1)
+	elif area_that_entered.is_in_group("energy"):
+		set_energy(energy + 20)
+	#energy += 20.0
+	#get_node("UI/EnergyBar").value = energy
