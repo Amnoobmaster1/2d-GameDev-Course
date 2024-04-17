91ee73f35aa059f01590f33c84b140f204283996
Amnoobmaster1 <159198065+Amnoobmaster1@users.noreply.github.com>
2024-04-17 11:24:07 -0700

    Update gem.gd

--- 05.loot_it_all_workbook/lessons/gem.gd
+++ 05.loot_it_all_workbook/lessons/gem.gd
@@ -3,10 +3,15 @@ extends Area2D
 func _on_area_entered(area_that_entered: Area2D) -> void:
 	queue_free()
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	area_entered.connect(_on_area_entered)
-
-
+	var tween := create_tween()
+	var sprite_2d := get_node("Sprite2D")
+	var target_position := Vector2(500, 400)
+	var animation_duration := 2.0
+	tween.tween_property(sprite_2d, "position", target_position, animation_duration)
+	tween.tween_property(sprite_2d, "scale", Vector2(2,2), 1.0)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
